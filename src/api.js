49b1ba5af025eb331113ac5c2be02e7c64bd1e8d
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-09 16:32:00 -0500

    another option

--- src/api.js
+++ src/api.js
@@ -1,44 +1,30 @@
-// module.exports = {
-//   getMovies: () => {
-//     fetch('/api/movies')
-//         .then(function (response) {
-//           // console.log(response.json());
-//           return response.json();
-//
-//         })
-//         .then(function (data) {
-//           appendData(data);
-//         })
-//         .catch(function (err) {
-//           console.log('error: ' + err);
-//         });
-//
-//     //Pushing data to HTML\\
-//     //Used above\\
-//     function appendData(data) {
-//       let movieContainer = document.getElementById("container");
-//       for (let i = 0; i < data.length; i++) {
-//         let div = document.createElement("div");
-//         div.innerHTML = 'Movie Name: ' + data[i].title + '<br>' +
-//             'Movie Rating: ' + data[i].rating;
-//         movieContainer.appendChild(div);
-//       }
-//     }
-//   }
-// };
+module.exports = {
+  getMovies: () => {
+      const url = ('/api/movies');
 
+    fetch(url)
+        .then(function (response) {
+          // console.log(response.json());
+          return response.json();
 
+        })
+        .then(function (data) {
+          appendData(data);
+        })
+        .catch(function (err) {
+          console.log('error: ' + err);
+        });
 
-
-// let pageLoad = () => {
-//
-//   $(window).on({
-//     ajaxStart: function () {
-//       $('.modal').text("Triggered ajaxStart handler.");
-//       $('body').addClass("loading");
-//     },
-//     ajaxStop: function () {
-//       $('body').removeClass("loading");
-//     }
-//   });
-// };
+    //Pushing data to HTML\\
+    //Used above\\
+    function appendData(data) {
+      let movieContainer = document.getElementById("container");
+      for (let i = 0; i < data.length; i++) {
+        let div = document.createElement("div");
+        div.innerHTML = 'Movie Name: ' + data[i].title + '<br>' +
+            'Movie Rating: ' + data[i].rating;
+        movieContainer.appendChild(div);
+      }
+    }
+  }
+};
--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
-// /**
-//  * es6 modules and imports
-//  */
-// import sayHello from './hello';
-// sayHello('World');
-
 /**
  * require style imports
  */
 
 const $ = require('jquery');
-// const {pageLoad} = require('./loading-message');
 
-// const {getMovies} = require('./add-new-movies');
-const {addNewMovie} = require('./add-new-movies');
-
-
-// getMovies();
-addNewMovie();
-
-
-//   function appendData(data) {
-//     let mainContainer = document.getElementById("myData");
-//     for (let i = 0; i < data.length; i++) {
-//       let div = document.createElement("div");
-//       div.innerHTML = 'Name: ' + data[i].firstName + ' ' + data[i].lastName;
-//       mainContainer.appendChild(div);
-//     }
-// }
 
 
 
