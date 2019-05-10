1526ad9eb64697f6c75c553fb1c8e2649e114ecd
Nadia Botello <botello.nadia.g@gmail.com>
2019-05-09 22:27:13 -0500

    more changes

--- src/api.js
+++ src/api.js
@@ -1,30 +1,85 @@
-module.exports = {
-  getMovies: () => {
-      const url = ('/api/movies');
+const url=('/api/movies');
 
-    fetch(url)
-        .then(function (response) {
-          // console.log(response.json());
-          return response.json();
 
-        })
-        .then(function (data) {
-          appendData(data);
-        })
-        .catch(function (err) {
-          console.log('error: ' + err);
-        });
+// returns json
 
-    //Pushing data to HTML\\
-    //Used above\\
-    function appendData(data) {
-      let movieContainer = document.getElementById("container");
-      for (let i = 0; i < data.length; i++) {
-        let div = document.createElement("div");
-        div.innerHTML = 'Movie Name: ' + data[i].title + '<br>' +
-            'Movie Rating: ' + data[i].rating;
-        movieContainer.appendChild(div);
-      }
-    }
-  }
+const getMovies = () => {
+    return fetch(url)
+        .then(response => response.json());
 };
+
+
+// add movie
+
+const addMovie =  () =>{
+    let title =$('#inputTitle').val();
+    let rating = $("input:radio[name=inlineRadioOptions]:checked").val();
+    let summary = $('.card-text').val();
+
+    const newMovie = {title: title, rating: rating, summary: summary};
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newMovie),
+    };
+    fetch(url,options)
+        .then(()=> console.log('added'))
+        .catch(()=> console.log('error'))
+};
+
+// remove movie
+
+const removeMovie = (movieID) => {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    fetch(url + '/' + movieID + '', options)
+        .then(() => console.log(`movie ${movieID} deleted`))
+        .catch(() => console.log('error on delete'));
+};
+
+
+// render
+function renderMovies() {
+    getMovies().then((movies) => {
+        $("#loadingicon").css("display", "none");
+
+        movies.forEach(({title, rating, id, summary, img}) => {
+            let html = ``;
+            html += `<div class="card flex-nowrap" style="width: 18rem;">`;
+            html += `<img src="` + img + `" class="card-img-top" alt="...">`;
+            html += `<div class="card-body">`;
+            html += `<h5 class="card-title">` + title + `</h5>`;
+            html += `<p class="card-text">` + summary + `</p>`;
+            html += `<p class="card-rating"><em>Rating: ` + rating + `</em></p>`;
+            html += `<a href="#" class="btn btn-secondary edit-button">Edit</a>`;
+            html += `<a href="#" class="btn btn-danger delete-button">Delete</a>`;
+            html += `</div>`;
+            html += `</div>`;
+            $('#pushFilm').append(html);
+        })
+    }).catch((error) => {
+        alert('Error.');
+        console.log(error);
+    })
+}
+
+
+
+
+function renderDeletePills() {
+    getMovies().then((movies) => {
+        movies.forEach(({title, rating, id}) => {
+            var html = `<a href="#" class="badge badge-danger" id="${id}">${title}</a>`;
+            $('#delete-mov-title').append(html);
+        })
+    })}
+
+
+
+export default {getMovies, addMovie, removeMovie, renderMovies, renderDeletePills};
--- src/index.js
+++ src/index.js
@@ -4,7 +4,43 @@
 
 const $ = require('jquery');
 
+import movies from './api.js';
 
+
+movies.renderMovies();
+
+
+
+$(".edit-button").on("click", function(){
+
+});
+
+$(".delete-button").on("click", function(){
+    movies.removeMovie();
+});
+
+$('#submitbutton').on('click', function(){
+    console.log($('#inputTitle').val());
+    console.log($("input:radio[name=inlineRadioOptions]:checked").val());
+    console.log($('#inputSummary').val());
+    movies.addMovie();
+    // $('#row1').html('');
+    movies.renderMovies();
+
+});
+
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "/api/movies",
+    "method": "GET",
+    "headers": {},
+    "data": "{}"
+};
+
+$.ajax(settings).done(function (response) {
+    console.log(response);
+});
 
 
 
