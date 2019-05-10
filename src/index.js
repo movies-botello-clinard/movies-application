767ae7cce628c87da6c83647604f36be17859426
Mykal Clinard <mykalclinard@gmail.com>
2019-05-10 16:06:32 -0500

    last

--- src/index.js
+++ src/index.js
@@ -52,6 +52,42 @@ $.ajax(settings).done(function (response) {
     console.log(response);
 });
 
+//
+// $(function(){
+//
+//     $('#Submitbutton').on('click', function(e){
+//         e.preventDefault();
+//         var imdbid  = $(this).attr('id');
+//         var resturl = "http://mymovieapi.com/?type=json&id="+imdbid+"&release=full&plot=full";
+//
+//         $('#imdbcontents').html('<center><img src="images/loading.gif" alt="loading..."></center>');
+//
+//         $.ajax({
+//             url: resturl,
+//             dataType: 'json',
+//             success: function(data){
+//                 var title  = data.title;
+//                 var genre  = data.genres[0];
+//                 var poster = data.poster.imdb;
+//                 var url    = data.imdb_url;
+//                 var plot   = data.plot;
+//                 var rlseyr = data.year;
+//
+//                 var cdiv = $('#imdbcontents');
+//
+//                 var thehtml = "<img src=\""+poster+"\" class=\"floatright\">";
+//                 thehtml = thehtml + "<h2>"+title+"</h2>";
+//                 thehtml = thehtml + "<p class=\"genre\">"+genre+" - first aired in "+rlseyr+"</p>";
+//                 thehtml = thehtml + "<p>"+plot+"</p>";
+//                 thehtml = thehtml + "<p><a href=\""+url+"\" target=\"_blank\">View on IMDb &rarr;</p>";
+//
+//                 cdiv.html(thehtml);
+//             }
+//         });
+//     });
+//
+// });
+
 
 
 
